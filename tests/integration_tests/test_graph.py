d70d38d1028e012e7b68f2e144bcd2ab9128114e
Aasher Kamal <aasherkamal786@gmail.com>
2025-02-13 21:00:31 +0500

    updated

--- tests/integration_tests/test_graph.py
+++ tests/integration_tests/test_graph.py
@@ -1,15 +1,2 @@
-import pytest
-from langsmith import unit
-
-from react_agent import graph
-
-
-@pytest.mark.asyncio
-@unit
-async def test_react_agent_simple_passthrough() -> None:
-    res = await graph.ainvoke(
-        {"messages": [("user", "Who is the founder of SpaceX?")]},
-        {"configurable": {"system_prompt": "You are a helpful AI assistant."}},
-    )
-
-    assert "elon musk" in str(res["messages"][-1].content).lower()
+"""This module contains the tests to ensure the Agents work as expected.
+"""
